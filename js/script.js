cdb4ffa7eb576a19bd6efb436099f1bb73913a43
mohammed-el-hayek <159185058+mohammed-el-hayek@users.noreply.github.com>
2024-03-18 02:47:42 +0000

    update first

--- js/script.js
+++ js/script.js
@@ -1,5 +1,10 @@
-// Copyright (c) 2020 Mr. Coxall All rights reserved
+// Copyright (c) 2024 Mohammed El-hayek All rights reserved
 //
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: Mohammed EL-hayek
+// Created on: Mar 2024
 // This file contains the JS functions for index.html
+
+function myButtonClicked() {
+ document.getElementById("multiply-math").innerHTML = "<p>The area is: " + (5 * 3) +"cm&sup2</p>"
+ document.getElementById("add-multiply-math").innerHTML = "<p>The perimeter is: " + (2 * 5 + 3) +"cm</p>"
+}
